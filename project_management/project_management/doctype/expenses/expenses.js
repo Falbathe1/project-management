62dd2fd493db3fa242c7673b60b4f4c0dde0624a
fouziawb <117811076+fouziawb@users.noreply.github.com>
2025-03-10 15:31:21 +0300

    Updated expenses.js to recalculate total expenses dynamically

--- project_management/project_management/doctype/expenses/expenses.js
+++ project_management/project_management/doctype/expenses/expenses.js
@@ -1,8 +1,21 @@
-// Copyright (c) 2025, pm and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Expenses", {
+    refresh: function(frm) {
+        calculate_total_expenses(frm);
+    },
+    expenses_items_add: function(frm, cdt, cdn) {
+        calculate_total_expenses(frm);
+    },
+    expenses_items_remove: function(frm) {
+        calculate_total_expenses(frm);
+    }
+});
 
-// frappe.ui.form.on("Expenses", {
-// 	refresh(frm) {
+function calculate_total_expenses(frm) {
+    let total = 0;
+    frm.doc.expenses_items.forEach(item => {
+        total += item.amount || 0; // Ensure it doesn’t break if amount is undefined
+    });
 
-// 	},
-// });
+    // Set the total in the form field
+    frm.set_value("total_expenses", total);
+}
